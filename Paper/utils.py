fdf4d483a7e0f53bea3e5dc6e7f2c3d49a08ad30
rehan-b <rehan.b@outlook.de>
2025-01-06 11:32:09 +0100

    ..

--- Paper/utils.py
+++ Paper/utils.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from lifelines import KaplanMeierFitter
 from lifelines import KaplanMeierFitter, WeibullAFTFitter
-from sksurv.metrics import concordance_index_ipcw
 from sksurv.util import Surv
 from sklearn.model_selection import train_test_split
 from class_DecisionTreeBaggingClassifier import DecisionTreeBaggingClassifier
@@ -37,55 +36,7 @@ def calculate_ijk_variance(
 
     bias_correction = n / B * np.sum(1-weights[weights > 0]) * np.var(T_N_b, axis=0, ddof=1)* np.sum(weights**2)
 
-    return biased_var_estimate, bias_correction
-
-
-def calculate_jk_after_bootstrap_variance(
-    clf: DecisionTreeBaggingClassifier,
-    X_pred_point: pd.DataFrame,
-    params_rf: dict,
-    df_train: pd.DataFrame,
-) -> float:
-    """
-    Calculates the Jackknife-after-Bootstrap variance (unbiased, if equal weights are used during bootstrapsampling)
-    for a given random forest classifier.
-    Parameters:
-        clf (DecisionTreeBaggingClassifier): The random forest classifier.
-        X_pred_point (pd.DataFrame): The input data point for prediction.
-        params_rf (dict): The parameters of the random forest.
-        df_train (pd.DataFrame): The training dataset.
-    Returns:
-        float: The Jackknife-after-Bootstrap variance.
-    """
-    n_samples = df_train.shape[0]
-
-    # Precompute predictions for all trees
-    tree_preds, theta = clf.predict_proba(X_pred_point.values)
-
-    # Cache the estimators' samples array for efficient reuse
-    estimators_samples = clf.boot_indices
-
-    # Prepare a boolean mask for each sample's presence in each estimator's bootstrap
-    presence_mask = np.zeros((n_samples, params_rf["n_estimators"]), dtype=bool)
-    for i, samples in enumerate(estimators_samples):
-        samples = np.array(samples, dtype=int)
-        presence_mask[samples, i] = True
-
-    theta_is = []
-    for ii in range(n_samples):
-        indices_without_ii = np.where(~presence_mask[ii])[0]
-        if 0 < len(indices_without_ii) < params_rf["n_estimators"]:
-            theta_is.append(tree_preds[indices_without_ii].mean())
-
-    theta_is = np.array(theta_is)
-    var_jka_biased = np.sum((theta_is - theta) ** 2) * (n_samples - 1) / n_samples
-
-    var_jka_correction = (
-        (np.exp(1) - 1)
-        * (n_samples / params_rf["n_estimators"])
-        * np.var(tree_preds, ddof=1)
-    )
-    return var_jka_biased - var_jka_correction
+    return biased_var_estimate-bias_correction
 
 
 def calculate_bootstrap_variance(
@@ -151,7 +102,6 @@ def calculate_bootstrap_variance(
     return np.var(preds, ddof=1)
 
 
-#########################################################################################################
 #########################################################################################################
 
 def create_weibull_data(params: dict, random_state: int) -> pd.DataFrame:
@@ -262,188 +212,168 @@ def ipc_weighted_mse(y_true, y_pred, sample_weight):
 
     return np.average((y_true - y_pred) ** 2, weights=sample_weight)
 
-def calculate_true_survival_probability(individual, params, t):
+def calculate_true_survival_probability(individual, params):
     (
         shape_weibull,
         scale_weibull_base,
-        b_bloodp,
-        b_diab,
-        b_age,
-        b_bmi,
-        b_kreat,
+        p_1,
+        p_2,
+        p_3,
+        p_4,
     ) = (
         params["shape_weibull"],
         params["scale_weibull_base"],
-        params["b_bloodp"],
-        params["b_diab"],
-        params["b_age"],
-        params["b_bmi"],
-        params["b_kreat"],
+        params["p_1"],
+        params["p_2"],
+        params["p_3"],
+        params["p_4"],
     )
 
     # Extrahieren der Kovariaten
-    bmi = individual['bmi'].values[0]
-    blood_pressure = individual['blood_pressure'].values[0]
-    kreatinkinase = individual['kreatinkinase'].values[0]
-    diabetes = individual['diabetes'].values[0]
-    age = individual['age'].values[0]
+    X_1 = individual['X_1'].values[0]
+    X_2 = individual['X_2'].values[0]
+    X_3 = individual['X_3'].values[0]
+    X_4 = individual['X_4'].values[0]
 
     # Berechnung des linearen Prädiktors (LP)
-    LP = (
-        b_bloodp * blood_pressure +
-        b_diab * diabetes +
-        b_age * age +
-        b_bmi * (bmi - 25)**2 +
-        b_kreat * np.log(kreatinkinase)
-    )
+    LP =  X_1 * p_1 +X_2 * p_2 + X_3 * p_3 +X_4 * p_4 
 
     # Individueller Skalenparameter lambda
     lambda_weibull = scale_weibull_base * np.exp(LP)
 
     # Überlebensfunktion
-    if shape_weibull == 1:
-        # Exponentialverteilung
-        S_t = np.exp(-t / lambda_weibull)
-    else:
-        # Weibull-Verteilung
-        S_t = np.exp(- (t / lambda_weibull) ** shape_weibull)
+    S_t = np.exp(- (params["tau"] / lambda_weibull) ** shape_weibull)
 
     return S_t
 
-#########################################################################################################
 #########################################################################################################
 
 def simulation(
     seed: int,
     data_generation_parameter: dict,
     params_rf: dict,
-    X_pred_point = pd.DataFrame({'X_1': [0], 'X_2': [1], 'X_3': [80], 'X_4': [40]}),
-):
+    train_models: bool ,
+    ijk_wager_calc: bool,
+    ijk_butt_calc: bool,
+    ijk2_butt_calc: bool, 
+    boot_calc: bool,
+    B_first_level: int ,
+    X_pred_point : pd.DataFrame,):
 
-    ### Erstellung des Datensatzes ###
     if data_generation_parameter['data_type'] == 'weibull':
         data = create_weibull_data(params=data_generation_parameter, random_state=seed)
-
-    # Startified Split in Train und Testdaten
-    df_train, df_test = stratified_split(data=data, random_state=seed, test_size=0.3)
-
-    # Erstellung der IPCW Gewichte
-    df_train = create_data_with_ipc_weights(data=df_train, params=data_generation_parameter)
-    df_test = create_data_with_ipc_weights(data=df_test, params=data_generation_parameter)
-    portion_zero_weights_train = df_train['weights_ipcw'].value_counts(normalize=True)[0]
+        df_train, df_test = stratified_split(data=data, random_state=seed, test_size=0.3)
+        df_train = create_data_with_ipc_weights(data=df_train, params=data_generation_parameter)
+        df_test = create_data_with_ipc_weights(data=df_test, params=data_generation_parameter)
+        portion_zero_weights_train = df_train['weights_ipcw'].value_counts(normalize=True)[0]
 
 
-    
     ### Weibull Modell ####
+    if train_models:
 
-    # Fit
-    aft = WeibullAFTFitter()
-    aft.fit(
-        df=df_train.drop(["weights_ipcw", "survived"], axis=1),
-        duration_col="time",
-        event_col="event",
-    )
-
-    # Evaluation auf Testdaten
-    y_pred = (
-        aft.predict_survival_function(
-            df=df_test.drop(["weights_ipcw", "survived", "time", "event"], axis=1),
-            times=data_generation_parameter["tau"],
+        # Fit
+        aft = WeibullAFTFitter()
+        aft.fit(
+            df=df_train.drop(["weights_ipcw", "survived"], axis=1),
+            duration_col="time",
+            event_col="event",
         )
-        .iloc[0]
-        .tolist()
-    )
 
-    wb_test_mse = ipc_weighted_mse(
-        y_true=df_test["survived"].values,
-        y_pred=y_pred,
-        sample_weight=df_test["weights_ipcw"],
-    )
+        # Evaluation auf Testdaten
+        y_pred = (
+            aft.predict_survival_function(
+                df=df_test.drop(["weights_ipcw", "survived", "time", "event"], axis=1),
+                times=data_generation_parameter["tau"],
+            )
+            .iloc[0]
+            .tolist()
+        )
+        wb_test_mse = ipc_weighted_mse(
+            y_true=df_test["survived"].values,
+            y_pred=y_pred,
+            sample_weight=df_test["weights_ipcw"],
+        )
 
-    # Prediction für X_erwartung
-    wb_pred_X_point = (
-        aft.predict_survival_function(df=X_pred_point, times=tau).iloc[0].tolist()
-    )
+        # Prediction für X_erwartung
+        wb_pred = (
+            aft.predict_survival_function(df=X_pred_point, times=data_generation_parameter["tau"]).iloc[0].tolist()
+        )
+    
 
-        ######################################### Random Forest Modell #########################################################################################
-    # Fitten des Random Forest Modells
-    params_rf["random_state"] = seed
-    clf = DecisionTreeBaggingClassifier(params_rf)
-    clf.fit(
-        X=df_train.drop(
-            ["time", "event", "weights_ipcw", "survived"], axis=1
-        ).values,
-        y=df_train["survived"].values,
-        sample_weights=df_train["weights_ipcw"].values,
-    )
+        ### Random Forest Modell ###
+        # Fit
+        params_rf["random_state"] = seed
+        clf = DecisionTreeBaggingClassifier(params_rf)
+        clf.fit(
+            X=df_train.drop(
+                ["time", "event", "weights_ipcw", "survived"], axis=1
+            ).values,
+            y=df_train["survived"].values,
+            sample_weights=df_train["weights_ipcw"].values,
+        )
 
-    _ , pred  =clf.predict_proba(df_test.drop(
-                ["weights_ipcw", "survived", "time", "event"], axis=1
-            ).values)
+        # Evaluation auf Testdaten
+        _ , pred  =clf.predict_proba(df_test.drop(
+            ["weights_ipcw", "survived", "time", "event"], axis=1
+        ).values)
+        rf_test_mse = ipc_weighted_mse(
+            y_true=df_test["survived"].values,
+            y_pred=pred,
+            sample_weight=df_test["weights_ipcw"].values,
+        )
 
-    # Evaluation auf Testdaten
-    rf_mse_ipcw = ipc_weighted_mse(
-        y_true=df_test["survived"].values,
-        y_pred=pred,
-        sample_weight=df_test["weights_ipcw"].values,
-    )
-    # Prediction für X_erwartung
-    _ ,rf_y_pred_X_point = clf.predict_proba(X_pred_point.values)
+        # Prediction für X_erwartung
+        _ ,rf_pred = clf.predict_proba(X_pred_point.values)
 
-   
-    wb_mse_ipcw = 0.0
-    wb_cindex_ipcw = 0.0
-    wb_y_pred_X_point = [0.0]
-    rf_mse_ipcw = 0.0
-    rf_y_pred_X_point = [0.0]
+    else:
+        wb_test_mse = 0.
+        rf_test_mse = 0.0
+        wb_pred = [0.0]
+        rf_pred = [0.0]
 
-    ######################################## Variance Estimation ##########################################################################################
+    #### Variance Estimation ####
+    ### Wager
+    if ijk_wager_calc:
+        ijk_wager_var = 0  #################### !!!!!!!!!!!!!!!!!!!
+    else:
+        ijk_wager_var = 0.0
 
-    ### IJK Variance Estimation WEIGHTED
-    if ijk_std_calc:
-        biased_var_estimate, bias_correction = calculate_ijk_variance(
+    ### Butt
+    if ijk_butt_calc:
+        ijk_butt_var = calculate_ijk_variance(
             clf=clf, X_pred_point=X_pred_point, df_train=df_train
         )
-
     else:
-        biased_var_estimate = 0.0
-        bias_correction = 0.0
+        ijk_butt_var = 0.0
 
-    ### Jackkknife after Bootstrap Variance Estimation UN-WEIGHTED
-    if jk_ab_calc:
-        jka_var_unbiased = calculate_jk_after_bootstrap_variance(
-            clf=clf, X_pred_point=X_pred_point, params_rf=params_rf, df_train=df_train
-        )
+    ### Butt2
+    if ijk2_butt_calc:
+        ijk2_butt_var = 0  #################### !!!!!!!!!!!!!!!!!!!
     else:
-        jka_var_unbiased = 0.0
+        ijk2_butt_var = 0.0
 
-    ### Bootstrap Variance Estimation WEIGHTED
-    if boot_std_calc:
-        bootstrap_var_pred_X_point = calculate_bootstrap_variance(
+    ### boot
+    if boot_calc:
+        boot_var = calculate_bootstrap_variance(
             X_pred_point=X_pred_point,
             params_rf=params_rf,
             df_train=df_train,
             seed=seed,
             B_first_level=B_first_level,
-            tau=tau,
+            tau=data_generation_parameter["tau"],
         )
     else:
-        bootstrap_var_pred_X_point = 0.0
+        boot_var = 0.0
 
     return (
-        portion_events_after_cut_train,
-        portion_censored_after_cut_train,
-        portion_no_events_after_cut_train,
-        portion_events_after_cut_test,
-        portion_censored_after_cut_test,
-        portion_no_events_after_cut_test,
-        wb_mse_ipcw,
-        wb_cindex_ipcw,
-        wb_y_pred_X_point,
-        rf_mse_ipcw,
-        rf_y_pred_X_point,
-        biased_var_estimate,
-        bias_correction,
-        bootstrap_var_pred_X_point,
-        jka_var_unbiased,
+        wb_test_mse,
+        rf_test_mse,
+        wb_pred,
+        rf_pred,
+        ijk_wager_var,
+        ijk_butt_var,
+        ijk2_butt_var,
+        boot_var,
+        portion_zero_weights_train,
+        calculate_true_survival_probability(X_pred_point, data_generation_parameter),
     )
